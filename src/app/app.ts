e5af50aaa61a420cfe5c6136b563fffd68c8f435
sebastienfernandez <sebastienfernandez@protonmail.com>
2026-02-21 22:03:35 +0100

    create two diffrerents counters

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('demo-evolucare');
+  score = signal(0);
+  points:number = 0;
+  count() {
+    this.score.set(this.score() + 1);
+  }
+  increment() {
+    this.points ++;
+  }
 }
